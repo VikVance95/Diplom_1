754d91635c3303f74e0e23fea21c0ba2cc6468f0
Artemii Eliseev <vikvance@mail.ru>
2025-01-02 00:52:17 +0300

    add unit tests + jacoco report

---
+++ src/test/java/IngredientParameterizedTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+
+public class IngredientTest {
+
+    @Test
+    public void getPriceTest() {
+
+    }
+
+    @Test
+    public void getNameTest() {
+
+    }
+
+    @Test
+    public void getTypeTest() {
+
+    }
+}
