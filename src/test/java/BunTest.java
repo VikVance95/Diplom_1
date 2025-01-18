01028a340e3fe2f1ebb38662dfeea34a3472be55
Artemii Eliseev <vikvance@mail.ru>
2025-01-19 00:26:31 +0300

    Update branch

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
 
 public class BunTest {
+    String name = "Булочка с маком";
+    float price = 56.99f;
+    Bun bun = new Bun(name, price);
+
+    @Test
+    public void getNameTest() {
+        assertEquals("Неверное название", name, bun.getName());
+    }
+
+    @Test
+    public void getPriceTest() {
+        assertEquals("Неверная цена", price, bun.getPrice(), 0);
+    }
 }
--- src/test/java/IngredientParameterizedTest.java
+++ src/test/java/IngredientParameterizedTest.java
@@ -1,20 +1,54 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
 
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
 
-public class IngredientTest {
+@RunWith(Parameterized.class)
+public class IngredientParameterizedTest {
+
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientParameterizedTest(IngredientType type, String name, float price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters(name = "Ингридиент {1} типа {0} за {2} денег")
+    public static Object[][] data() {
+        return new Object[][] {
+                {FILLING, "chili sauce", 100},
+                {SAUCE, "hot sauce", 150},
+                {null, "cutlet", 100},
+                {FILLING, "cutlet", 100},
+                {FILLING, "sausage", 300},
+                {SAUCE, "sour cream", 200},
+                {FILLING, "", 0},
+        };
+    }
 
     @Test
     public void getPriceTest() {
-
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals("Ожидалась другая цена", price, ingredient.getPrice(), 0);
     }
 
     @Test
     public void getNameTest() {
-
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals("Ожидалось другое название ингредиента", name, ingredient.getName());
     }
 
     @Test
     public void getTypeTest() {
-
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals("Ожидался другой тип ингредиента", type, ingredient.getType());
     }
 }
